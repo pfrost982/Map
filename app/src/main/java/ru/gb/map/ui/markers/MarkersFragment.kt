ad52b0cea9c8d930f1f902c27dedd6151d97ff06
pfrost982 <pfrost982@gmail.com>
2022-08-19 18:08:36 +0700

    Initial commit

---
+++ app/src/main/java/ru/gb/map/ui/markers/MarkersFragment.kt
@@ -0,0 +1,39 @@
+package ru.gb.map.ui.markers
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import ru.gb.map.databinding.FragmentMarkersBinding
+
+class MarkersFragment : Fragment() {
+
+    private var _binding: FragmentMarkersBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val markersViewModel =
+            ViewModelProvider(this).get(MarkersViewModel::class.java)
+
+        _binding = FragmentMarkersBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textMarkers
+        markersViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/ru/gb/map/ui/markers/MarkersViewModel.kt
@@ -0,0 +1,13 @@
+package ru.gb.map.ui.markers
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MarkersViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is markers Fragment"
+    }
+    val text: LiveData<String> = _text
+}
